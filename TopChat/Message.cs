3e6dbd94bade6d8c7e8ceb31ee567958a7b3ac94
TopAcademy2022 <popov_r_top@outlook.com>
2025-01-21 20:49:45 +0300

    Add simple p2p messenger.

---
+++ TopChat/Message.cs
@@ -0,0 +1,90 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace TopChat
+{
+	public class Message
+	{
+		private const ushort _DATA_SIZE = 1024;
+
+		private byte[] _data;
+
+		private delegate void AddMessage();
+
+		private delegate void PrintMessage();
+
+		private event AddMessage _DataChanged;
+
+		private event PrintMessage _WriteStop;
+
+		private string _userLogin;
+
+		private IPEndPoint _destinationIP;
+
+		Socket _listenSocket;
+
+		private List<string> _messageList;
+
+		public Message(Socket listenSocket, string userLogin, IPEndPoint destinationIP)
+		{
+			this._data = new byte[_DATA_SIZE];
+			this._DataChanged += this.AddToMessages;
+
+			this._listenSocket = listenSocket;
+
+			this._userLogin = userLogin;
+			this._destinationIP = destinationIP;
+
+			this._messageList = new List<string>();
+			this._WriteStop += this.PrintAllMessages;
+		}
+
+		private async Task Listen()
+		{
+			int size = await this._listenSocket.ReceiveAsync(this._data);
+
+			if (size > 0)
+			{
+				this._DataChanged?.Invoke();
+			}
+		}
+
+		public void Write()
+		{
+			Console.WriteLine("Введите сообщение для отправки:");
+			string data = Console.ReadLine();
+			this._listenSocket.SendTo(Encoding.ASCII.GetBytes(
+				$"{DateTime.Now}.{this._userLogin}: {data}"),
+				this._destinationIP);
+
+			this._WriteStop?.Invoke();
+		}
+
+		public void StartListen()
+		{
+			Task.Run(() => this.Listen());
+		}
+
+		private void AddToMessages()
+		{
+			this._messageList.Add(this.ToString());
+		}
+
+		private void PrintAllMessages()
+		{
+			Thread.Sleep(1000);
+			foreach (string message in this._messageList)
+			{
+				Console.WriteLine($"Вам отправлено: {message}");
+			}
+
+			this._messageList.Clear();
+		}
+
+		public override string ToString()
+		{
+			return Encoding.UTF8.GetString(this._data);
+		}
+	}
+}
--- TopChat/Program.cs
+++ TopChat/Program.cs
@@ -1,1 +1,33 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System.Net;
+using System.Net.Sockets;
+using TopChat;
+
+const ushort APP_PORT = 5000;
+
+Console.WriteLine("Введите IP ПК, которому будете отправлять запросы:");
+string destinationIP = Console.ReadLine();
+
+Console.WriteLine("Введите ваш логин:");
+string userLogin = Console.ReadLine();
+
+// Задать адрес, куда будет отправлять наши сообщения
+IPAddress iPAddress = IPAddress.Parse(destinationIP);
+// Задать точку назначения (на какой IP отправлять данные)
+IPEndPoint destinationEndPoint = new IPEndPoint(iPAddress.Address, APP_PORT);
+
+// Создать объект класса Socket для работы по сети
+Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+// Задать точку назначения (наш IP для прослушивания сообщений)
+IPEndPoint myIP = new IPEndPoint(IPAddress.Any, APP_PORT);
+// Привязаться к точке назначения (работать с ней)
+socket.Bind(myIP);
+
+while (true)
+{
+	Message message = new Message(socket, userLogin, destinationEndPoint);
+	message.StartListen();
+	message.Write();
+}
+
+// Завершить работу по сети
+socket.Close();

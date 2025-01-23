802f9a373fa7735395ee74749229e32ed9126704
Kasyanov-Igor <igor.kasyanov00@mail.ru>
2025-01-23 21:20:52 +0300

    Added Database working. Closed #6

---
+++ TopChat/Services/Interfaces/ADatabaseConnection.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TopChat.Models;
+
+namespace TopChat.Services.Interfaces
+{
+	public abstract class ADatabaseConnection : DbContext
+	{
+		protected abstract string ReturnConnectionString();
+
+		protected string ConnectionString { get; private set; }
+
+		public DbSet<User> Users => Set<User>();
+		public DbSet<NetworkData> NetworkDatas => Set<NetworkData>();
+
+		public DbSet<Message> Messages => Set<Message>();
+
+		public ADatabaseConnection()
+		{
+			this.ConnectionString = this.ReturnConnectionString();
+			this.Database.EnsureCreated();
+		}
+	}
+}
---
+++ TopChat/Services/MongoDBConnection.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TopChat.Services.Interfaces;
+
+namespace TopChat.Services
+{
+    public class MongoDBConnection : ADatabaseConnection
+    {
+        public const string _DATABASE_NAME = "../TopChat.db";
+
+        protected override string ReturnConnectionString()
+        {
+            return $"Data Source={_DATABASE_NAME}";
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseMongoDB(this.ConnectionString, "TopChat");
+        }
+    }
+}

515a0b0c8b32183feec40043d14854b91f7991ab
TopAcademy2022 <popov_r_top@outlook.com>
2025-01-23 20:50:37 +0300

    Added project files. Closed #1

---
+++ TopChat/Services/Interfaces/IDataConverterService.cs
@@ -0,0 +1,9 @@
+﻿using TopChat.Models;
+
+namespace TopChat.Services.Interfaces
+{
+	public interface IDataConverterService
+	{
+		public NetworkData Convert<T>(T fromEntity, NetworkData toEntity) where T : class;
+	}
+}

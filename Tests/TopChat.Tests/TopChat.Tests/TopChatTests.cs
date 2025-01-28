44b3d2d668b859b932d31eb6045b18c3f17d5ed1
artemqdh <artjom-05@mail.ru>
2025-01-28 20:12:38 +0300

    Added tests for ConnectionProviderUdp. Closed #24

--- Tests/TopChat.Tests/TopChat.Tests/TopChatTests.cs
+++ Tests/TopChat.Tests/TopChat.Tests/TopChatTests.cs
@@ -1,11 +1,26 @@
+using TopChat.Services;
+
 namespace TopChat.Tests
 {
     public class TopChatTests
     {
         [Fact]
-        public void Test1()
+        public void Test_SetDestination()
         {
+            ConnectionProviderUdp connectionProviderUdp = new ConnectionProviderUdp();
 
+            Assert.True(connectionProviderUdp.SetDestination("127.0.0.1", 12345));
+        }
+
+        [Fact]
+        public void Test_SendUdp()
+        {
+            ConnectionProviderUdp connectionProviderUdp = new ConnectionProviderUdp();
+            Assert.True(connectionProviderUdp.SetDestination("127.0.0.1", 12345));
+
+            byte[] buffer_test = new byte[1024];
+
+            Assert.True(connectionProviderUdp.Send(buffer_test));
         }
     }
 }

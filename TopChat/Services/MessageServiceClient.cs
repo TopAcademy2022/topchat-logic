bf08084da30b764ccb0c258f2c7acf4c92a8e4c6
TopAcademyBel <121047497+TopAcademy2022@users.noreply.github.com>
2025-01-28 19:36:49 +0300

    Added IMessageService. Closed #22 (#23)

---
+++ TopChat/Services/MessageServiceClient.cs
@@ -0,0 +1,31 @@
+﻿using TopChat.Models;
+using TopChat.Services.Interfaces;
+
+namespace TopChat.Services
+{
+    public class MessageServiceClient : IMessageService
+    {
+        private IDataConverterService _iDataConverterService;
+
+        private INetworkDataService _iNetworkDataService;
+
+        public MessageServiceClient(IDataConverterService iDataConverterService,
+            INetworkDataService iNetworkDataService)
+        {
+            this._iDataConverterService = iDataConverterService;
+            this._iNetworkDataService = iNetworkDataService;
+        }
+
+        public bool AddMessage(Message message)
+        {
+            NetworkData networkData = this._iDataConverterService.ConvertToNetworkData(message);
+            return this._iNetworkDataService.Send(networkData);
+        }
+
+        public List<Message> GetMessages(User sender)
+        {
+            NetworkData networkData = this._iNetworkDataService.Get(this._iNetworkDataService.CreateRequest(sender, true));
+            return this._iDataConverterService.ConvertFromNetworkData<List<Message>>(networkData);
+        }
+    }
+}

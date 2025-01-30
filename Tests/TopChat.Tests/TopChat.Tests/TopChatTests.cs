4a1aa05ad79df7e0eded6d8be5d0efffbc349a4d
Igor <161635179+Kasyanov-Igor@users.noreply.github.com>
2025-01-30 19:06:39 +0300

    Added Database working. Closed #6 (#32)

---
+++ Tests/TopChat.Tests/TopChat.Tests/TopChatTests.cs
@@ -0,0 +1,25 @@
+using Microsoft.EntityFrameworkCore;
+using MongoDB.Driver.Core.Configuration;
+using TopChat.Services;
+using TopChat.Services.Interfaces;
+
+namespace TopChat.Tests
+{
+	public class TopChatTests
+	{
+		[Fact]
+		public void SqliteConnectionTest()
+		{
+			SqliteConnection connection = new SqliteConnection();
+
+			Assert.True(connection.Database.CanConnect());
+		}
+
+		//[Fact]
+		//public void MongoDBConnectionTest()
+		//{
+		//	MongoDBConnection connection = new MongoDBConnection();
+		//	Assert.True(connection.Database.CanConnect());
+		//}
+	}
+}

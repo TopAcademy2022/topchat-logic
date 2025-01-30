ee78ff73d2bb6311069623f73b69b5d16f0144c1
natalia21082000 <161637129+natalia21082000@users.noreply.github.com>
2025-01-30 20:56:47 +0300

    Added tests for UserService. Closed #34

---
+++ Tests/TopChat.Tests/TopChat.Tests/UserServesTests.cs
@@ -0,0 +1,29 @@
+﻿using TopChat.Services;
+using TopChat.Services.Interfaces;
+
+namespace TopChat.Tests
+{
+    public class UserServesTests
+    {
+        private ADatabaseConnection _db;
+        public UserServesTests()
+        {
+            this._db = new SqliteConnection();
+        }
+        [Fact]
+        public void RegistrationTest()
+        {
+            UserServes userServes = new UserServes(_db);
+            string testLogin = "testUser";
+            string testPassword = "testPassword";
+
+            userServes.Registration(testPassword, testLogin);
+
+            Assert.Single(_db.Users);
+            Assert.Equal(testLogin, _db.Users.First().Login); 
+            Assert.Equal(testPassword, _db.Users.First().Password);
+
+            _db.Database.EnsureDeleted();
+        }
+    }
+}

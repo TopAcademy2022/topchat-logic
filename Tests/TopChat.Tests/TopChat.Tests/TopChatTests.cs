9dd5dc2981468d0398a188c98124feec2e0205ac
artemqdh <artjom-05@mail.ru>
2025-01-30 18:41:26 +0300

    Squashed commit of the following:
    commit 494dc852172802fe5dbf6d89ad8f6abd6ff2d0cb
    Author: artemqdh <artjom-05@mail.ru>
    Date:   Wed Jan 29 19:02:01 2025 +0300
    
        updated tests
    
    # Conflicts:
    #	Tests/TopChat.Tests/TopChat.Tests/TopChatTests.cs

--- Tests/TopChat.Tests/TopChat.Tests/TopChatTests.cs
+++ Tests/TopChat.Tests/TopChat.Tests/TopChatTests.cs
@@ -1,3 +1,5 @@
+using System.Net;
+using System.Net.Sockets;
 using TopChat.Services;
 
 namespace TopChat.Tests
@@ -5,22 +7,31 @@ namespace TopChat.Tests
     public class TopChatTests
     {
         [Fact]
-        public void Test_SetDestinationUdp()
+        public void SetDestinationUdpTest()
         {
             ConnectionProviderUdp connectionProviderUdp = new ConnectionProviderUdp();
-
             Assert.True(connectionProviderUdp.SetDestination("127.0.0.1", 12345));
+
+            Assert.True(connectionProviderUdp.GetIPEndPoint().Address.ToString() == "127.0.0.1");
+            Assert.True(connectionProviderUdp.GetIPEndPoint().Port == 12345);
         }
 
         [Fact]
-        public void Test_SendUdp()
+        public void SendUdpTest()
         {
-            ConnectionProviderUdp connectionProviderUdp = new ConnectionProviderUdp();
-            Assert.True(connectionProviderUdp.SetDestination("127.0.0.1", 12345));
+            using UdpClient receiver = new UdpClient(12345);
+            receiver.Client.ReceiveTimeout = 1000;
 
-            byte[] buffer_test = new byte[1024];
+            ConnectionProviderUdp sender = new ConnectionProviderUdp();
+            Assert.True(sender.SetDestination("127.0.0.1", 12345));
 
-            Assert.True(connectionProviderUdp.Send(buffer_test));
+            byte[] testData = { 1,2,3,4,5 };
+            Assert.True(sender.Send(testData));
+
+            IPEndPoint remoteEP = new IPEndPoint(IPAddress.Any, 0);
+            byte[] receivedData = receiver.Receive(ref remoteEP);
+
+            Assert.Equal(testData, receivedData);
         }
     }
 }

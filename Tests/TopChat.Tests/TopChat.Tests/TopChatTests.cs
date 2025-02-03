c07d12f61cdbf7d7fb7e988cdb8c54cd55e2cb8b
Kasyanov-Igor <igor.kasyanov00@mail.ru>
2025-02-03 19:12:46 +0300

    Added tests for DependencyInjectionSetup. Closed #36

--- Tests/TopChat.Tests/TopChat.Tests/TopChatTests.cs
+++ Tests/TopChat.Tests/TopChat.Tests/TopChatTests.cs
@@ -1,20 +1,22 @@
 using System.Net;
 using System.Net.Sockets;
-using Microsoft.EntityFrameworkCore;
-using MongoDB.Driver.Core.Configuration;
 using TopChat.Services;
-using TopChat.Services.Interfaces;
 
 namespace TopChat.Tests
 {
 	public class TopChatTests
 	{
+		private SqliteConnection _connection;
+
+		public TopChatTests()
+		{
+			this._connection = new SqliteConnection();
+		}
+
 		[Fact]
 		public void SqliteConnectionTest()
 		{
-			SqliteConnection connection = new SqliteConnection();
-
-			Assert.True(connection.Database.CanConnect());
+			Assert.True(this._connection.Database.CanConnect());
 		}
 
 		//[Fact]
@@ -23,33 +25,33 @@ namespace TopChat.Tests
 		//	MongoDBConnection connection = new MongoDBConnection();
 		//	Assert.True(connection.Database.CanConnect());
 		//}
-    
-    [Fact]
-    public void SetDestinationUdpTest()
-    {
-        ConnectionProviderUdp connectionProviderUdp = new ConnectionProviderUdp();
-        Assert.True(connectionProviderUdp.SetDestination("127.0.0.1", 12345));
 
-        Assert.True(connectionProviderUdp.GetIPEndPoint().Address.ToString() == "127.0.0.1");
-        Assert.True(connectionProviderUdp.GetIPEndPoint().Port == 12345);
-    }
+		[Fact]
+		public void SetDestinationUdpTest()
+		{
+			ConnectionProviderUdp connectionProviderUdp = new ConnectionProviderUdp();
+			Assert.True(connectionProviderUdp.SetDestination("127.0.0.1", 12345));
 
-    [Fact]
-    public void SendUdpTest()
-    {
-        using UdpClient receiver = new UdpClient(12345);
-        receiver.Client.ReceiveTimeout = 1000;
+			Assert.True(connectionProviderUdp.GetIPEndPoint().Address.ToString() == "127.0.0.1");
+			Assert.True(connectionProviderUdp.GetIPEndPoint().Port == 12345);
+		}
 
-        ConnectionProviderUdp sender = new ConnectionProviderUdp();
-        Assert.True(sender.SetDestination("127.0.0.1", 12345));
+		[Fact]
+		public void SendUdpTest()
+		{
+			using UdpClient receiver = new UdpClient(12345);
+			receiver.Client.ReceiveTimeout = 1000;
 
-        byte[] testData = { 1,2,3,4,5 };
-        Assert.True(sender.Send(testData));
+			ConnectionProviderUdp sender = new ConnectionProviderUdp();
+			Assert.True(sender.SetDestination("127.0.0.1", 12345));
 
-        IPEndPoint remoteEP = new IPEndPoint(IPAddress.Any, 0);
-        byte[] receivedData = receiver.Receive(ref remoteEP);
+			byte[] testData = { 1, 2, 3, 4, 5 };
+			Assert.True(sender.Send(testData));
 
-        Assert.Equal(testData, receivedData);
-    }
+			IPEndPoint remoteEP = new IPEndPoint(IPAddress.Any, 0);
+			byte[] receivedData = receiver.Receive(ref remoteEP);
+
+			Assert.Equal(testData, receivedData);
+		}
 	}
 }

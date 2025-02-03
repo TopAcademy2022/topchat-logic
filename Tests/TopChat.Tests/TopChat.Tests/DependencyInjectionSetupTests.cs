48e2f7471e2758a950763d782857e071ad9692ea
Igor <161635179+Kasyanov-Igor@users.noreply.github.com>
2025-02-03 21:29:17 +0300

    Added tests for DependencyInjectionSetup. Closed #36 (#38)

---
+++ Tests/TopChat.Tests/TopChat.Tests/DependencyInjectionSetupTests.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using TopChat.Services.Interfaces;
+
+
+namespace TopChat.Tests
+{
+	public class DependencyInjectionSetupTests
+	{
+		private ADatabaseConnection? _connection;
+
+		private IMessageService? _messageService;
+
+		private IConnectionProvider? _connectionProvider;
+
+		private IGroupService? _grouping;
+
+		private INetworkDataService? _networkDataService;
+
+		private IUserServes? _userServes;
+
+		~DependencyInjectionSetupTests() => this._connection.Database.EnsureDeleted();
+
+		[Fact]
+		public void DependencyInjectionTest()
+		{
+			ServiceCollection services = new ServiceCollection();
+
+			TopChat.Infrastructure.DependencyInjectionSetup.ConfigureServices(services);
+
+			using var serviceProvider = services.BuildServiceProvider();
+
+			this._connection = serviceProvider.GetService<ADatabaseConnection>();
+
+			this._messageService = serviceProvider.GetService<IMessageService>();
+
+			this._connectionProvider = serviceProvider.GetService<IConnectionProvider>();
+
+			this._grouping = serviceProvider.GetService<IGroupService>();
+
+			this._networkDataService = serviceProvider.GetService<INetworkDataService>();
+
+			this._userServes = serviceProvider.GetService<IUserServes>();
+
+			Assert.True(this._messageService.GetType().Name == "MessageServiceClient");
+			Assert.True(this._connection.GetType().Name == "SqliteConnection");
+			Assert.True(this._connectionProvider.GetType().Name == "ConnectionProviderUdp");
+			Assert.True(this._networkDataService.GetType().Name == "NetworkDataService");
+			Assert.True(this._userServes.GetType().Name == "UserServes");
+			Assert.True(this._grouping.GetType().Name == "GroupService");
+		}
+	}
+}
--- Tests/TopChat.Tests/TopChat.Tests/TopChatTests.cs
+++ Tests/TopChat.Tests/TopChat.Tests/TopChatTests.cs
@@ -1,7 +1,5 @@
 using System.Net;
 using System.Net.Sockets;
-using Microsoft.EntityFrameworkCore;
-using MongoDB.Driver.Core.Configuration;
 using TopChat.Services;
 using TopChat.Services.Interfaces;
 
@@ -9,13 +7,17 @@ namespace TopChat.Tests
 {
 	public class TopChatTests
 	{
-		[Fact]
-		public void SqliteConnectionTest()
-		{
-			SqliteConnection connection = new SqliteConnection();
+		//protected ADatabaseConnection _connection;
 
-			Assert.True(connection.Database.CanConnect());
-		}
+		//public TopChatTests() => this._connection = new SqliteConnection();
+
+		//~TopChatTests() => this._connection.Database.EnsureDeleted();
+
+		//[Fact]
+		//public void SqliteConnectionTest()
+		//{
+		//	Assert.True(this._connection.Database.CanConnect());
+		//}
 
 		//[Fact]
 		//public void MongoDBConnectionTest()
@@ -23,33 +25,33 @@ namespace TopChat.Tests
 		//	MongoDBConnection connection = new MongoDBConnection();
 		//	Assert.True(connection.Database.CanConnect());
 		//}
-    
-    [Fact]
-    public void SetDestinationUdpTest()
-    {
-        ConnectionProviderUdp connectionProviderUdp = new ConnectionProviderUdp();
-        Assert.True(connectionProviderUdp.SetDestination("127.0.0.1", 12345));
 
-        Assert.True(connectionProviderUdp.GetIPEndPoint().Address.ToString() == "127.0.0.1");
-        Assert.True(connectionProviderUdp.GetIPEndPoint().Port == 12345);
-    }
+		[Fact]
+		public void SetDestinationUdpTest()
+		{
+			ConnectionProviderUdp connectionProviderUdp = new ConnectionProviderUdp();
+			Assert.True(connectionProviderUdp.SetDestination("127.0.0.1", 12345));
 
-    [Fact]
-    public void SendUdpTest()
-    {
-        using UdpClient receiver = new UdpClient(12345);
-        receiver.Client.ReceiveTimeout = 1000;
+			Assert.True(connectionProviderUdp.GetIPEndPoint().Address.ToString() == "127.0.0.1");
+			Assert.True(connectionProviderUdp.GetIPEndPoint().Port == 12345);
+		}
 
-        ConnectionProviderUdp sender = new ConnectionProviderUdp();
-        Assert.True(sender.SetDestination("127.0.0.1", 12345));
+		[Fact]
+		public void SendUdpTest()
+		{
+			using UdpClient receiver = new UdpClient(12345);
+			receiver.Client.ReceiveTimeout = 1000;
 
-        byte[] testData = { 1,2,3,4,5 };
-        Assert.True(sender.Send(testData));
+			ConnectionProviderUdp sender = new ConnectionProviderUdp();
+			Assert.True(sender.SetDestination("127.0.0.1", 12345));
 
-        IPEndPoint remoteEP = new IPEndPoint(IPAddress.Any, 0);
-        byte[] receivedData = receiver.Receive(ref remoteEP);
+			byte[] testData = { 1, 2, 3, 4, 5 };
+			Assert.True(sender.Send(testData));
 
-        Assert.Equal(testData, receivedData);
-    }
+			IPEndPoint remoteEP = new IPEndPoint(IPAddress.Any, 0);
+			byte[] receivedData = receiver.Receive(ref remoteEP);
+
+			Assert.Equal(testData, receivedData);
+		}
 	}
 }
